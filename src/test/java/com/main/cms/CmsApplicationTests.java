bec340d5fe03ca6dc18d4d21a8905fc52788e6a8
Moung Chanda <chanda.m@bidc.com.kh>
2026-05-23 09:44:17 +0700

    init code

---
+++ src/test/java/com/main/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
